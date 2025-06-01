8c3d210f1aad0b6bbfed6864204ac26f2eeff854
ZagoXD <128556675+ZagoXD@users.noreply.github.com>
2025-06-01 20:15:55 -0300

    att routers

--- frontend/src/routers/AppRouter.jsx
+++ frontend/src/routers/AppRouter.jsx
@@ -8,6 +8,15 @@ import App from "../App";
 import LoginPage from "../pages/LoginPage";
 import CadastroPage from "../pages/CadastroPage";
 import PoliticaPrivacidadePage from "../pages/PoliticaPrivacidadePage";
+import ProtectedRoute from "./ProtectedRoute";
+import MinhaAreaLayout from "../layouts/MinhaAreaLayout";
+import AdminRoute from "./AdminRoute";
+import ListarAtividadesAdminPage from "../pages/admin/activities/ListarAtividadesAdminPage";
+import CriarEditarAtividadePage from "../pages/admin/activities/CriarEditarAtividadePage";
+import ListarEstudantesPage from "../pages/superadmin/users/ListarEstudantesPage";
+import EditarEstudantePage from "../pages/superadmin/users/EditarEstudantePage";
+import ListarAdminsPage from "../pages/superadmin/users/ListarAdminsPage";
+import CriarEditarAdminPage from "../pages/superadmin/users/CriarEditarAdminPage";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +27,59 @@ const router = createBrowserRouter([
       { path: "login", element: <LoginPage /> },
       { path: "cadastro", element: <CadastroPage /> },
       { path: "politica-de-privacidade", element: <PoliticaPrivacidadePage /> },
+
+      {
+        element: <ProtectedRoute />, 
+        children: [
+          {
+            path: "minha-area",
+            element: <MinhaAreaLayout />,
+            children: [
+              { index: true, element: <Navigate to="perfil" replace /> },
+              {
+                path: "inscricoes",
+              },
+              {
+                element: <AdminRoute />, 
+                children: [
+                  {
+                    path: "admin/atividades",
+                    element: <ListarAtividadesAdminPage />,
+                  },
+                  {
+                    path: "admin/atividades/nova",
+                    element: <CriarEditarAtividadePage />,
+                  },
+                  {
+                    path: "admin/atividades/:activityId/editar",
+                    element: <CriarEditarAtividadePage />,
+                  },
+                  {
+                    path: "superadmin/usuarios/estudantes",
+                    element: <ListarEstudantesPage />,
+                  },
+                  {
+                    path: "superadmin/usuarios/estudantes/:userId/editar",
+                    element: <EditarEstudantePage />,
+                  },
+                  {
+                    path: "superadmin/usuarios/admins", 
+                    element: <ListarAdminsPage />,
+                  },
+                  {
+                    path: "superadmin/usuarios/admins/nova",
+                    element: <CriarEditarAdminPage />,
+                  },
+                  {
+                    path: "superadmin/usuarios/admins/:userId/editar",
+                    element: <CriarEditarAdminPage />,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
     ],
   },
   {
